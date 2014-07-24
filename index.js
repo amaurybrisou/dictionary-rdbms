5a60f88ad81a19d73b4c5c91171ef7aaee6a3224
amaurybrisou <amaury.brisou@gmail.com>
2014-07-24 16:15:09 +0200

    new tests

--- index.js
+++ index.js
@@ -1,130 +1,168 @@
 var internals = {}
 
-internals.models = function(sequelize, Sequelize, next){
+internals.models = function (sequelize, Sequelize, next) {
   var models = {}
 
-  var Country = 
-  models.Country = sequelize.define('Country', {
-    country: {
-      type : Sequelize.STRING,
-      notEmpty: true,
-      notNull: true,
-      unique: true
-    }
-  }, { "timestamps" : true })
-
-  var Definition = 
-  models.Definition = sequelize.define('Definition', {
-    definition: {
-      type: Sequelize.TEXT,
-      notNull: true,
-      notEmpty: true,
-      unique: true
-    },
-    uid: {
-      type : Sequelize.STRING,
-      unique: true,
-      validate: {
+  var Country =
+    models.Country = sequelize.define('Country', {
+      country: {
+        type: Sequelize.STRING,
+        notEmpty: true,
         notNull: true,
-        isUUID: 4
+        unique: true
       }
-    }
-  }, { "timestamps" : true })
+    }, {
+      "timestamps": true
+    })
 
-  var Example = 
-  models.Example = sequelize.define('Example', {
-    example: {
-      type: Sequelize.TEXT,
-      notNull: true,
-      notEmpty: true,
-      unique: true
-    }
-  }, { "timestamps" : true })
+  var Definition =
+    models.Definition = sequelize.define('Definition', {
+      definition: {
+        type: Sequelize.TEXT,
+        notNull: true,
+        notEmpty: true,
+        unique: true
+      },
+      uid: {
+        type: Sequelize.STRING,
+        unique: true,
+        validate: {
+          notNull: true,
+          isUUID: 4
+        }
+      }
+    }, {
+      "timestamps": true
+    })
 
-  var Hyperlink = 
-  models.Hyperlink = sequelize.define('Hyperlink', {
-    hyperlink: {
-      type: Sequelize.STRING,
-      isUrl: true,
-      notNull: true,
-      notEmpty: true,
-      unique: true
-    }
-  }, { "timestamps" : true })
+  var Example =
+    models.Example = sequelize.define('Example', {
+      example: {
+        type: Sequelize.TEXT,
+        notNull: true,
+        notEmpty: true,
+        unique: true
+      }
+    }, {
+      "timestamps": true
+    })
 
-  var Word = 
-  models.Word = sequelize.define('Word', {
-    lema: {
-      type: Sequelize.STRING,
-      notNull: true,
-      notEmpty: true,
-      unique: 'lema-pos-index'
-    },
-    pos: {
-      type: Sequelize.STRING,
-      notNull: true,
-      notEmpty: true,
-      unique: 'lema-pos-index'
-    },
-    gerund: {
-      type: Sequelize.STRING,
-      notNull: false,
-      notEmpty: true,
-      // unique: 'lema-pos-index'
-    },
-    participle: {
-      type: Sequelize.STRING,
-      notNull: false,
-      notEmpty: true,
-      // unique: 'lema-pos-index'
-    },
-    register: {
-      type: Sequelize.BOOLEAN,
-      notNull: true,
-      defaultValue: true,
-      notEmpty: true,
-    }
-  }, { "timestamps" : true })
+  var Hyperlink =
+    models.Hyperlink = sequelize.define('Hyperlink', {
+      hyperlink: {
+        type: Sequelize.STRING,
+        isUrl: true,
+        notNull: true,
+        notEmpty: true,
+        unique: true
+      }
+    }, {
+      "timestamps": true
+    })
 
-  var Language = 
-  models.Language = sequelize.define('Language', {
-    language: {
-      type: Sequelize.STRING,
-      isAlpha: true,
-      notNull: true,
-      notEmpty: true,
-      unique: true
-    }
-  } , { "timestamps" : true })
+  var Word =
+    models.Word = sequelize.define('Word', {
+      lema: {
+        type: Sequelize.STRING,
+        notNull: true,
+        notEmpty: true,
+        unique: 'lema-pos-index'
+      },
+      pos: {
+        type: Sequelize.STRING,
+        notNull: true,
+        notEmpty: true,
+        unique: 'lema-pos-index'
+      },
+      gerund: {
+        type: Sequelize.STRING,
+        notNull: false,
+        notEmpty: true,
+        // unique: 'lema-pos-index'
+      },
+      participle: {
+        type: Sequelize.STRING,
+        notNull: false,
+        notEmpty: true,
+        // unique: 'lema-pos-index'
+      },
+      register: {
+        type: Sequelize.BOOLEAN,
+        notNull: true,
+        defaultValue: true,
+        notEmpty: true,
+      }
+    }, {
+      "timestamps": false
+    })
 
-  var WordCountry = 
-  models.WordCountry = sequelize.define('WordCountry', {
-     frequency: {
-      type: Sequelize.INTEGER.UNSIGNED,
-      notNull: true,
-      defaultValue: 0,
-      notEmpty: true,
-      min: 0,
-      max: 100
-    }
-  }, { "timestamps" : false })
+  var Language =
+    models.Language = sequelize.define('Language', {
+      language: {
+        type: Sequelize.STRING,
+        isAlpha: true,
+        notNull: true,
+        notEmpty: true,
+        unique: true
+      }
+    }, {
+      "timestamps": true
+    })
 
-  Word.hasMany(Country, { through: WordCountry , onDelete: 'CASCADE'})
-  Country.hasMany(Word, { through: WordCountry, onDelete: 'CASCADE'})
+  var WordCountry =
+    models.WordCountry = sequelize.define('WordCountry', {
+      frequency: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        notNull: true,
+        defaultValue: 0,
+        notEmpty: true,
+        min: 0,
+        max: 100
+      }
+    }, {
+      "timestamps": false
+    })
+
+  Word.hasMany(Country, {
+    through: WordCountry,
+    onDelete: 'CASCADE'
+  })
+  Country.hasMany(Word, {
+    through: WordCountry,
+    onDelete: 'CASCADE'
+  })
 
 
-  Word.hasMany(Word, { as : 'Synonyms', joinTableName: 'Synonyms'})
-  Word.hasMany(Word, { as : 'Antonyms', joinTableName: 'Antonyms'})
-  Word.hasMany(Word, { as : 'Relatives', joinTableName: 'Relatives'})
-  Word.hasMany(Word, { as : 'Compounds', joinTableName: 'Compounds'})
+  Word.hasMany(Word, {
+    as: 'Synonyms',
+    joinTableName: 'Synonyms',
+  })
+  Word.hasMany(Word, {
+    as: 'Antonyms',
+    joinTableName: 'Antonyms'
+  })
+  Word.hasMany(Word, {
+    as: 'Relatives',
+    joinTableName: 'Relatives',
+  })
+  Word.hasMany(Word, {
+    as: 'Compounds',
+    joinTableName: 'Compounds'
+  })
 
   Language.hasMany(Word)
   Word.belongsTo(Language)
-  
-  Word.hasMany(Hyperlink, {onDelete: 'CASCADE'})
 
-  Word.hasMany(Definition, {onDelete: 'CASCADE'})
-  Definition.hasMany(Example, {onDelete: 'CASCADE'})
+  Word.hasMany(Hyperlink, {
+    onDelete: 'CASCADE'
+  })
+
+  Word.hasMany(Definition, {
+    onDelete: 'CASCADE'
+  })
+  Definition.hasMany(Example, {
+    onDelete: 'CASCADE'
+  })
 
   sequelize.models = models
 
@@ -132,53 +170,73 @@ internals.models = function(sequelize, Sequelize, next){
 
 }
 
-internals.install = function(sequelize, options, next){
+internals.install = function (sequelize, options, next) {
 
   sequelize
-  .sync(options)
-  .complete(function(err) {
-    if (!!err) {
-      next(err)
-    }
+    .sync(options)
+    .complete(function (err) {
+      if (!!err) {
+        next(err)
+      }
 
 
-    sequelize.models.Language.bulkCreate([
-      { language: "Spanish" },
-      { language: "English" }
-    ])
-    .error(function(err){
-      next(err)
-    })
-    .success(function(){
-      sequelize.models.Country.bulkCreate([
-        { country: "Spain" },
-        { country: "Mexico" },
-        { country: "Costa Rica" },
-        { country: "Peru" },
-        { country: "Colombia" },
-        { country: "Ecuador" },
-        { country: "Guatemala" },
-        { country: "Cuba" },
-        { country: "Honduras" },
-        { country: "Panama" },
-        { country: "Equatorial Guinea" },
-        { country: "Puerto Rico" },
-        { country: "Paraguay" },
-        { country: "Argentina" },
-        { country: "Chile" },
-        { country: "Nicaragua" },
-        { country: "Urugay" },
-        { country: "Dominican Republic" },
-        { country: "Bolivia" }
-
-      ])
-      .error(function(err){
-        if (!!err) {
+      sequelize.models.Language.bulkCreate([{
+        language: "Spanish"
+      }, {
+        language: "English"
+      }])
+        .error(function (err) {
           next(err)
-        }
-      })
+        })
+        .success(function () {
+          sequelize.models.Country.bulkCreate([{
+              country: "Spain"
+            }, {
+              country: "Mexico"
+            }, {
+              country: "Costa Rica"
+            }, {
+              country: "Peru"
+            }, {
+              country: "Colombia"
+            }, {
+              country: "Ecuador"
+            }, {
+              country: "Guatemala"
+            }, {
+              country: "Cuba"
+            }, {
+              country: "Honduras"
+            }, {
+              country: "Panama"
+            }, {
+              country: "Equatorial Guinea"
+            }, {
+              country: "Puerto Rico"
+            }, {
+              country: "Paraguay"
+            }, {
+              country: "Argentina"
+            }, {
+              country: "Chile"
+            }, {
+              country: "Nicaragua"
+            }, {
+              country: "Urugay"
+            }, {
+              country: "Dominican Republic"
+            }, {
+              country: "Bolivia"
+            }
+
+          ])
+            .error(function (err) {
+              if (!!err) {
+                next(err)
+              }
+            })
+        })
     })
-  })
 
   return next(null, sequelize)
 }
@@ -191,40 +249,40 @@ exports.register = function (plugin, options, next) {
 
   var sequelize_plugin = plugin.servers[0].plugins['hapi-sequelize']
 
-  if(!sequelize_plugin){
-    plugin.log(['dictionary-rdbms', 'fatal'],'hapi-sequelize plugin required')
+  if (!sequelize_plugin) {
+    plugin.log(['dictionary-rdbms', 'fatal'], 'hapi-sequelize plugin required')
     process.exit(1)
   }
 
-  if(sequelize_plugin.sequelize && sequelize_plugin.Sequelize){
+  if (sequelize_plugin.sequelize && sequelize_plugin.Sequelize) {
     var sequelize = sequelize_plugin.sequelize,
-        Sequelize = sequelize_plugin.Sequelize
+      Sequelize = sequelize_plugin.Sequelize
 
-    if(options.drop){
-      internals.models(sequelize, Sequelize, function(err, sequelize){
-        if(err){
+    if (options.drop) {
+      internals.models(sequelize, Sequelize, function (err, sequelize) {
+        if (err) {
           plugin.log(['dictionary-rdbms', 'error'], err)
         }
-        internals.install(sequelize, options.sync, function(err, sequelize){
-          if(err){
+        internals.install(sequelize, options.sync, function (err, sequelize) {
+          if (err) {
             plugin.log(['dictionary-rdbms', 'error'], err)
           }
 
-          plugin.servers.forEach(function(server){
+          plugin.servers.forEach(function (server) {
             plugin.expose('db', sequelize)
             plugin.expose('models', sequelize.models)
           })
-        }) 
+        })
       });
     } else {
 
-      internals.models(sequelize, Sequelize, function(err, sequelize){
-        if(err){
+      internals.models(sequelize, Sequelize, function (err, sequelize) {
+        if (err) {
           console.log(err)
           plugin.log(['dictionary-rdbms', 'error'], err)
         }
 
-        plugin.servers.forEach(function(server){
+        plugin.servers.forEach(function (server) {
           plugin.expose('db', sequelize)
           plugin.expose('models', sequelize.models)
         })
@@ -233,11 +291,10 @@ exports.register = function (plugin, options, next) {
   }
 
   return next()
-  
+
 }
 
 exports.register.attributes = {
   name: 'dictionary-rdbms',
   pkg: require('./package.json')
 };
-
